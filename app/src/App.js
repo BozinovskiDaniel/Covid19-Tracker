b1b5122e5f5d733be6595374cd49655160edbe0a
BozinovskiDaniel <BozinovskiDaniel@hotmail.com>
2020-05-15 09:23:17 +1000

    added global theme file

--- app/src/App.js
+++ app/src/App.js
@@ -5,17 +5,26 @@ import home from "./pages/home";
 import Navbar from "./components/layout/Navbar";
 import individualCountry from "./pages/individualCountry";
 
+// Material UI
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import themeFile from "./util/theme";
+
+const theme = createMuiTheme(themeFile);
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route path="/country/:countryName" component={individualCountry} />
-        </Switch>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route path="/country/:countryName" component={individualCountry} />
+          </Switch>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ app/src/util/theme.js
@@ -0,0 +1,16 @@
+export default {
+  palette: {
+    primary: {
+      light: "#32CD32",
+      main: "#404040",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+};
